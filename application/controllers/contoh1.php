6107a8fcf42cc383eca8072ebf767dbc9c2390da
Haryani <haryaniharyani047@gmail.com>
2023-09-26 19:17:22 +0700

    tugas1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+    {
+     echo "<h1> Perkenalkan</h1>";
+     echo "Nama saya Haryani
+     Saya tinggal di daerah Jakarta Pusat
+     Saya suka sama Satria";
+    }
+}
