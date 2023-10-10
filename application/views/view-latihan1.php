2262c7c35c56dc16446dcc1d2b3191a477d33cfa
Haryani <haryaniharyani047@gmail.com>
2023-10-10 19:37:00 +0700

    pertemuan 3

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    halo kawan .. Yuk kita belajar web programing....!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metodhe penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2. " = " . $hasil; ?>
+</body>
+
+</html>
