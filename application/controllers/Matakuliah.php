0edefb6c63e991451602ec85963c6e3807826ba5
Haryani <haryaniharyani047@gmail.com>
2023-10-24 18:43:13 +0700

    PERTEMUAN 6

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -11,7 +11,21 @@ class Matakuliah extends CI_controller
     
     public function cetak()
     {
-
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah',
+        'required|min_length[3]', [
+         'required' => 'Kode Matakuliah Harus diisi',
+         'min_lenght' => 'Kode terlalu pendek'
+         ]);
+         $this->form_validation->set_rules('nama', 'Nama Matakuliah',
+        'required|min_length[3]', [
+         'required' => 'Nama Matakuliah Harus diisi',
+         'min_lenght' => 'Nama terlalu pendek'
+         ]);
+         
+         if ($this->form_validation->run() != true) {
+         $this->load->view('view-form-matakuliah');
+         } else {
+        
        $data = [
         'kode' => $this->input->post('kode'),
         'nama' => $this->input->post('nama'),
@@ -19,6 +33,7 @@ class Matakuliah extends CI_controller
 
        ];
 
-       $this->load->view('view-data-matakuliah', $data);
+         $this->load->view('view-data-matakuliah', $data);
+       }
     }
 }
